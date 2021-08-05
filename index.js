c2fa0e26179be9d3c7eee120d0041d537bad13d7
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-06 08:42:41 +0900

    Questions and stdout

---
+++ index.js
@@ -0,0 +1,10 @@
+const questions = [
+    "What did you learn today?",
+    "What bothered you today?",
+    "What should I do to improve?",
+    "What made me happy?",
+    "Did I help anyone today?How many?"
+
+]
+
+process.stdout.write("Alguma coisa")  //escreve algo que sairá
